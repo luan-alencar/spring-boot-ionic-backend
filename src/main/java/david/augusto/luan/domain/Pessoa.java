e270182bb2e8da9a1aeb4267b6460ea9f33b0615
luan-alencar <luanalencar134@gmail.com>
2020-12-06 12:48:12 -0300

    primeiro commit

---
+++ src/main/java/david/augusto/luan/domain/Pessoa.java
@@ -0,0 +1,34 @@
+package david.augusto.luan.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="contact_name")
+	private String nome;
+	@Column(name="contact_email")
+	private String email;
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
