1b3728701bb464834e5f367cedb8ff44367a6402
luan-alencar <luanalencar134@gmail.com>
2020-12-06 16:27:52 -0300

    Testando no Postman uma requisição GET de Categoria

--- src/main/java/david/augusto/luan/resources/CategoriaResource.java
+++ src/main/java/david/augusto/luan/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package david.augusto.luan.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import david.augusto.luan.domain.Categoria;
+
 @RestController
-@RequestMapping("/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "REST rodando!";
+	public List<Categoria> list() {
+
+		Categoria c1 = new Categoria(1L, "Higiene");
+		Categoria c2 = new Categoria(2L, "Petiscos");
+
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(c1);
+		categorias.add(c2);
+		
+		return categorias;
 	}
 }
