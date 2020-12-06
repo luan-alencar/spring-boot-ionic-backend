2f396bbcda17c7be23ca164425a1917dd54ed77e
luan-alencar <luanalencar134@gmail.com>
2020-12-06 12:58:35 -0300

    Pesquisa no db

--- src/main/java/david/augusto/luan/domain/Programa.java
+++ src/main/java/david/augusto/luan/domain/Programa.java
@@ -7,18 +7,17 @@ import javax.persistence.Persistence;
 public class Programa {
 
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pessoa");
-		EntityManager em = 	emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
+		EntityManager em = emf.createEntityManager();
+
+		// pesquisa no banco de dados
+		Pessoa p = em.find(Pessoa.class, 3L);
+		System.out.println(p);
+
 		System.out.println("Pronto!");
+		emf.close();
+		em.close();
 	}
 
 }
