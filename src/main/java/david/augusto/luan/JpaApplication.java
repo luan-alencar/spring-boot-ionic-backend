0db2e3d29c0cb7ef3455a62903c252ffed18c887
luan-alencar <luanalencar134@gmail.com>
2020-12-07 09:00:06 -0300

    Criando operação de instanciação

--- src/main/java/david/augusto/luan/JpaApplication.java
+++ src/main/java/david/augusto/luan/JpaApplication.java
@@ -1,13 +1,31 @@
 package david.augusto.luan;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import david.augusto.luan.domain.Categoria;
+import david.augusto.luan.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class JpaApplication {
+public class JpaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		// salvando as categorias no db
+		repository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
