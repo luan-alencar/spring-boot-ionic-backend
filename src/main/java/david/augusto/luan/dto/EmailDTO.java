147a3835f877d99b995015c3d915f6a11951f7be
luan-alencar <luanalencar134@gmail.com>
2021-01-04 15:09:45 -0300

    Esqueci a senha

---
+++ src/main/java/david/augusto/luan/dto/EmailDTO.java
@@ -0,0 +1,17 @@
+package david.augusto.luan.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Data;
+
+@Data
+public class EmailDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@NotEmpty(message = "Preenchimento obrigatório!")
+	@Email(message = "Email inválido!")
+	private String email;
+}
--- src/main/java/david/augusto/luan/resources/AuthResource.java
+++ src/main/java/david/augusto/luan/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package david.augusto.luan.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import david.augusto.luan.dto.EmailDTO;
 import david.augusto.luan.security.JWTUtil;
 import david.augusto.luan.security.UserSS;
+import david.augusto.luan.services.AuthService;
 import david.augusto.luan.services.UserService;
 
 @RestController
@@ -18,12 +22,21 @@ public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
-	
+
+	@Autowired
+	private AuthService service;
+
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
