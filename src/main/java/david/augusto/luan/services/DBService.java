27b7ab127ef0717961ad000d0e41565948231583
Luan <luanalencar134@gmail.com>
2021-01-14 22:52:07 -0300

    Adicionando dependencias

--- src/main/java/david/augusto/luan/services/DBService.java
+++ src/main/java/david/augusto/luan/services/DBService.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
--- src/main/java/david/augusto/luan/services/SmtpEmailService.java
+++ src/main/java/david/augusto/luan/services/SmtpEmailService.java
@@ -4,21 +4,20 @@ import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import david.augusto.luan.domain.Pedido;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class SmtpEmailService extends AbstractEmailService{
 
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 	
-	@Autowired
 	private MailSender mailSender;
 	
-	@Autowired
 	private JavaMailSender javaMailSender;
 	
 	
